8664e8fdcf2149b52e49acbfefe8c930e07f4e92
aashish_k <aashishkathat00@gmail.com>
2025-02-08 04:08:25 +0530

    trpc setup

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,16 @@
+import { trpc } from '@/trpc/client';
+import { HydrateClient } from '@/trpc/server';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+
 export default function Page() {
-   return <div>Home Page</div>;
+   // const { data } = trpc.hello.useQuery({ text: 'aash' });
+
+   return (
+      <HydrateClient>
+         <Suspense fallback={<p>loading..</p>}>
+            {/* <ErrorBoundary></ErrorBoundary> */}
+         </Suspense>
+      </HydrateClient>
+   );
 }
