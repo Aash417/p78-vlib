261471f899fbbc37d9fd9d1878c374ec210eef5f
aashish_k <aashishkathat00@gmail.com>
2025-02-07 23:49:24 +0530

    added auth

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = { children: React.ReactNode };
+
+export default function Layout({ children }: Props) {
+   return (
+      <div className="min-h-screen flex items-center justify-center">
+         {children}
+      </div>
+   );
+}
--- src/modules/home/ui/components/home-sidebar/main-section.tsx
+++ src/modules/home/ui/components/home-sidebar/main-section.tsx
@@ -7,6 +7,7 @@ import {
    SidebarMenuButton,
    SidebarMenuItem,
 } from '@/components/ui/sidebar';
+import { useAuth, useClerk } from '@clerk/nextjs';
 import { FlameIcon, HomeIcon, PlaySquareIcon } from 'lucide-react';
 import Link from 'next/link';
 
@@ -22,6 +23,9 @@ const items = [
 ];
 
 export default function MainSection() {
+   const clerk = useClerk();
+   const { isSignedIn } = useAuth();
+
    return (
       <SidebarGroup>
          <SidebarGroupContent>
@@ -32,7 +36,12 @@ export default function MainSection() {
                         tooltip={item.title}
                         asChild
                         isActive={false} //todo change at pathname
-                        onClick={() => {}}
+                        onClick={(e) => {
+                           if (!isSignedIn && item.auth) {
+                              e.preventDefault();
+                              return clerk.openSignIn();
+                           }
+                        }}
                      >
                         <Link
                            href={item.url}
