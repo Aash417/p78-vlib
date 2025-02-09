631aef5f47419575b0837cb2f24a24b789730581
aashish_k <aashishkathat00@gmail.com>
2025-02-09 09:35:42 +0530

    infinite loading

--- src/modules/studio/ui/view/studio-view.tsx
+++ src/modules/studio/ui/view/studio-view.tsx
@@ -2,8 +2,16 @@ import VideosSection from '../sections/videos-section';
 
 export default function StudioView() {
    return (
-      <div>
-         <VideosSection />
+      <div className="flex flex-col gap-y-6 pt-2.5">
+         <div className="px-4">
+            <h1 className="text-2xl font-bold">Channel content</h1>
+
+            <p className="text-xs text-muted-foreground">
+               Mangage your channel & videos
+            </p>
+
+            <VideosSection />
+         </div>
       </div>
    );
 }
